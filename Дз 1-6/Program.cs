a84432a307a4e402b1403bb8506373e0adf50c75
samat <samat@SAMATGG>
2023-09-28 23:30:57 +0300

    Добавьте файлы проекта.

---
+++ Дз 1-6/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace Дз_1_6
+{
+    enum Days //Для упр 4
+    {
+        Понедельник,
+        Вторник, 
+        Среда,
+        Четверг,
+        Пятница,
+        Суббота,
+        Воскресенье
+    }
+    enum Cards //Для упр 2
+    {
+        Шестерка,
+        Семерка,
+        Восьмерка,
+        Девятка,
+        Десятка,
+        Валет,
+        Дам,
+        Король,
+        Туз
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Упр 1. - 10 чисел. Определить, является ли эта последовательность упорядоченной по возрастанию,в случае отриц. ответа
+            // определить порядковый номер первого числа, который нарушает этот порядок
+            Console.WriteLine("Упр 1. - 10 чисел. Определить, является ли эта последовательность упорядоченной по возрастанию\n");
+            int i = 0;            
+            bool flag = true;
+            int[] numbers = new int[10];
+            Console.WriteLine("Введите первое число массива");
+            do
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                if (i > 0 && numbers[i] <= numbers[i - 1])
+                {
+                    flag = false;
+                    break;
+                }
+                
+            } while (i++ < 9);
+            if (flag)
+            {
+                Console.WriteLine("Последовательность упорядочена");
+            }
+            else
+            {
+                Console.WriteLine($"Порядок сбился на числе - {numbers[i]}, его индекс - {i}");
+            }
+            // Упр 2 - Номера и названия карт, использовать try-catch-finally
+            Console.WriteLine("\nУпр 2 - Номера и названия карт, использовать try-catch-finally\n");
+            Console.WriteLine("Введите число от 6 до 14");
+            int nuumber;
+            bool a = int.TryParse(Console.ReadLine(), out nuumber);
+            if (a)
+            {
+                switch (nuumber)
+                {
+                    case 6:
+                        Console.WriteLine($"6 - это {Cards.Шестерка}");
+                        break;
+                    case 7:
+                        Console.WriteLine($"7 - это {Cards.Семерка}");
+                        break;
+                    case 8:
+                        Console.WriteLine($"8 - это {Cards.Восьмерка}");
+                        break;
+                    case 9:
+                        Console.WriteLine($"9 - это {Cards.Девятка}");
+                        break;
+                    case 10:
+                        Console.WriteLine($"10 - это {Cards.Десятка}");
+                        break;
+                    case 11:
+                        Console.WriteLine($"11 - это {Cards.Валет}");
+                        break;
+                    case 12:
+                        Console.WriteLine($"12 - это {Cards.Дам}");
+                        break;
+                    case 13:
+                        Console.WriteLine($"13 - это {Cards.Король}");
+                        break;
+                    case 14:
+                        Console.WriteLine($"14 - это {Cards.Туз}");
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели ненужное число");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неправильно введено число");
+            }
+
+            // Упр 3. - На входе строку и производит выходные данные в соответствии с таблицей
+            Console.WriteLine("\nУпр 3. На входе строку и производит выходные данные в соответствии с таблицей\n");
+            Console.WriteLine("Кто вы?");
+            string profession = Console.ReadLine().ToLower();
+            switch (profession)
+            {
+                case ("jabroni"):
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case ("school Counselor"):
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case ("programmer"):
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case ("bike Gang Members"):
+                    Console.WriteLine("Moonshine");
+                    break;
+                case ("politican"):
+                    Console.WriteLine("Yout taxi dollars");
+                    break;
+                case ("rapper"):
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+
+
+            // Упр 4. Порядковый номер недели и его название (использовать enum)
+            Console.WriteLine("\nУпр 4. Порядковый номер недели и его название (использовать enum)\n");
+            Console.WriteLine("Введите число от 1 до 7");
+            int day ;
+            bool chislo = int.TryParse(Console.ReadLine(), out day);
+            if (chislo)
+            {
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine($"{day} - {Days.Понедельник}");
+                            break;
+                    case 2:
+                        Console.WriteLine($"{day} - {Days.Вторник}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{day} - {Days.Среда}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{day} - {Days.Четверг}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{day} - {Days.Пятница}");
+                        break;
+                    case 6:
+                        Console.WriteLine($"{day} - {Days.Суббота}");
+                        break;
+                    case 7:
+                        Console.WriteLine($"{day} - {Days.Воскресенье}");
+                        break;
+                    default:
+                        Console.WriteLine("Ваше число не подходит данному диапазону");
+                        break;
+                }   
+            }
+            else
+            {
+                Console.WriteLine("Неправильно ввели число");
+            }
+
+
+            // Упр 5 - создать массив, искать в нем при помощи foreach 'Barbie doll' or 'Hello Kitty'
+            int bag = 0;
+            Console.WriteLine("\nУпр 5 - создать массив, искать в нем при помощи foreach 'Barbie doll' or 'Hello Kitty'\n");
+            string[] dolls = new string[] { "Dogs", "Cats", "Barbie doll", "rats", "proga", "crash", "Hello Kitty", "comp", "Barbie doll" };
+            foreach ( string doll in dolls ) 
+            { 
+                if (doll == "Barbie doll" || doll == "Hello Kitty")
+                {
+                    bag++;
+                }
+            }
+            Console.WriteLine($"Общее количество нужных нам игрушек - {bag}");
+        }
+    }
+}
---
+++ Тумаков Лабораторка 4/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+
+
+namespace Тумаков_Лабораторка_4
+{
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ДЗ Тумакова, Упр 4.1 лабораторка - написать число от 1 до 365, определить какой это день месяца и месяц
+            //Учесть високосный год или нет
+            Console.WriteLine("Упр 4.1 - написать число от 1 до 365, определить какой это день месяца и месяц\n");
+            Console.WriteLine("Введите число от 1 до 365");
+            int day;
+            bool chislo = int.TryParse(Console.ReadLine(), out day);
+            if (chislo)
+            {
+                if (day >= 1 && day <= 365)
+                {
+                    if (day >= 1 && day <= 31)
+                    {
+                        Console.WriteLine($"Число {day} соотвествует {day} {Months.January}");
+                    }
+                    else if (day >= 32 && day <= 59)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 31} {Months.February}");
+                    }
+                    else if (day >= 60 && day <= 90)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 59} {Months.March}");
+                    }
+                    else if (day >= 91 && day <= 120)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 90} {Months.April}");
+                    }
+                    else if (day >= 121 && day <= 151)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 120} {Months.May}");
+                    }
+                    else if (day >= 152 && day <= 181)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 151} {Months.June}");
+                    }
+                    else if (day >= 182 && day <= 212)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 181} {Months.July}");
+                    }
+                    else if (day >= 213 && day <= 243)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 212} {Months.August}");
+                    }
+                    else if (day >= 244 && day <= 273)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 243} {Months.September}");
+                    }
+                    else if (day >= 274 && day <= 304)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 273} {Months.October}");
+                    }
+                    else if (day >= 305 && day <= 334)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 304} {Months.November}");
+                    }
+                    else if (day >= 335 && day <= 365)
+                    {
+                        Console.WriteLine($"Число {day} соответствует {day - 334} {Months.December}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не то число, которое нужно"); // Упр 4.2 - если число меньше 1 или больше 365 выработать исключение
+                }
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели число неправильно");
+            }
+
+            
+            // Д.з 4.1 - Учесть високосный год или нет
+            Console.WriteLine("\nД.з 4.1 - Учесть високосный год или нет\n");
+            Console.WriteLine("Введите число от 1 до 366");
+            int day1, year;
+            bool chislo1 = int.TryParse(Console.ReadLine(), out day1);
+            Console.WriteLine("Введите год");
+            bool yr = int.TryParse(Console.ReadLine(), out year);
+            if (yr)
+            {
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                {
+                    Console.WriteLine("Год високосный");
+                    if (chislo1)
+                    {
+                        if (day1 >= 1 && day1 <= 366)
+                        {
+                            if (day1 >= 1 && day1 <= 31)
+                            {
+                                Console.WriteLine($"Число {day1} соответсвует {day1} {Months.January}");
+                            }
+                            else if (day1 >= 32 && day1 <= 60)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 32} {Months.February}");
+                            }
+                            else if (day1 >= 61 && day1 <= 91)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 60} {Months.March}");
+                            }
+                            else if (day1 >= 92 && day <= 121)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 91} {Months.April}");
+                            }
+                            else if (day1 >= 122 && day <= 152)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 121} {Months.May}");
+                            }
+                            else if (day1 >= 153 && day1 <= 182)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 152} {Months.June}");
+                            }
+                            else if (day1 >= 183 && day <= 213)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 182} {Months.July}");
+                            }
+                            else if (day1 >= 214 && day1 <= 244)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 213} {Months.August}");
+                            }
+                            else if (day1 >= 245 && day1 <= 274)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 244} {Months.September}");
+                            }
+                            else if (day1 >= 275 && day1 <= 305)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 274} {Months.October}");
+                            }
+                            else if (day1 >= 306 && day1 <= 335)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 305} {Months.November}");
+                            }
+                            else if (day1 >= 336 && day <= 366)
+                            {
+                                Console.WriteLine($"Число {day1} соответствует {day1 - 335} {Months.December}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели не то число, которое нужно");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неправильно ввели число");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Год невисокосный");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неправильно ввели год");
+            }
+        }
+    }
+}
